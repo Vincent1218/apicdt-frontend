7851d40ac954e66c3157f92a74354339183a1fae
Vincent1218 <vicentchai@hotmail.com>
2021-08-01 17:37:31 +0800

    update

--- src/views/MatchUpTable.js
+++ src/views/MatchUpTable.js
@@ -9,8 +9,10 @@ const MatchUpTable = () => {
     <div>
       <div className="matchTitle">正赛</div>
       <img src={image} alt="MatchUpTable" className="matchUpTable"  width="85%" />
+      <div className = "matchUpWinner">工行杯-第十届亚太大专华语辩论公开赛总冠军：华南理工大学</div>
       <div className="matchTitle">返尔赛</div>
       <img src={imageFan} alt="MatchUpTable" className="matchUpTable"  width="85%" />
+      <div className = "matchUpWinner">工行杯-亚太特别企划-返尔赛总冠军：如果夺冠能不能拿常年杯</div>
       <Footer/>
     </div>
   )
